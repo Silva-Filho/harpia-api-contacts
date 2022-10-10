d2f569bacb7a8d7f45f9a0fab0465fac415964dc
Silva Filho <omega_silva@yahoo.com.br>
2022-10-10 14:51:18 -0300

    feat: cria middleware de captura de erro.
    Cria middleware para capturar erro de validação e de acesso a rota
    restrita sem o token.

---
+++ src/middlewares/handleError.js
@@ -0,0 +1,20 @@
+const { ValidationError } = require( "express-validation" );
+const { UnauthorizedError } = require( "express-jwt" );
+
+// eslint-disable-next-line no-unused-vars
+const handleError = async ( error, req, res, next ) => {
+    if ( error instanceof ValidationError ) {
+        return res.status( error.statusCode ).json( error );
+    }
+
+    // @ts-ignore
+    if ( error instanceof UnauthorizedError ) {
+        return res.status( error.status ).json( error );
+    }
+
+    return res.status( 500 ).json( error );
+};
+
+module.exports = {
+    handleError,
+};
