c08790bdcadc0b7e9ee39aeaf2d6d6a1a419d2ee
Silva Filho <tarusilva@gmail.com>
2022-10-14 19:29:51 -0300

    feat: adiciona o CORS ao server

--- src/server.js
+++ src/server.js
@@ -2,19 +2,25 @@
 require( "dotenv" ).config( { path: "./src/env/.env" } );
 
 const express = require( "express" );
+const cors = require( "cors" );
 
 const { db } = require( "./database/connection" );
 const { handleError } = require( "./middlewares/handleError" );
 const { router } = require( "./router" );
 
-const port = 8081;
+// const port = 8081;
+// eslint-disable-next-line no-undef
+const port = process.env.PORT;
 
 const server = express();
 
 db.hasConnection();
 
 server.use( express.json() );
+server.use( cors() );
 server.use( router );
+// server.use( "/api", require( "./router" ) );
+// server.use( "/api", router );
 
 server.use( handleError );
 
